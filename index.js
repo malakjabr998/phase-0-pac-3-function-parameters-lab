14321e75ccd96335273f818f7c9385c316ec64aa
malak jabr <malakjabr2@gmail.com>
2023-10-06 22:29:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name){
+    return (`Hi, my name is ${name}.`)
+}
+introduction("malak")
+// second function
+function introductionWithLanguage(name, language){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+introductionWithLanguage("Aki", "Ember.js")
+//third function
+function introductionWithLanguageOptional(name, language="JavaScript"){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+introductionWithLanguageOptional("Aki", "Ember.js")
+
